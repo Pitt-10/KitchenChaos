d679bbfc943e555021395f5e647e4e507835c0bd
Pitt-10 <pedrogutman2010@gmail.com>
2026-08-21 09:03:56 -0300

    boxCollider terminado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,13 +13,43 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        float playerRadius = .6f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
 
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if (!canMove) {
+            // no me puedo mover en direccion diagonal
+
+            //intento de moverte en x
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove) {
+                //te podes mover solo en x
+                moveDir = moveDirX;
+            }
+            else {
+                //no te podes mover en x
+            }
+        }
         if (canMove) {
             transform.position += moveDir * moveSpeed * Time.deltaTime;
         }
-        isWalking = moveDir != Vector3.zero;
+        else {
+            //intento de moverte en y
+            Vector3 moveDirZ = new Vector3(moveDir.z, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+            if (canMove) {
+                //te podes mover solo en z
+                moveDir = moveDirZ;
+            }
+            else { 
+                //no te podes mover en ninguna direccion
+            }
+        }
+            isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
